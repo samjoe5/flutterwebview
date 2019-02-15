a0ef8bb784106d8123dd2f50c4fcd96bf56a0ef5
samjoe5 <43204242+samjoe5@users.noreply.github.com>
2019-02-15 15:33:23 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,153 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:google_sign_in/google_sign_in.dart';
-//import 'package:flutter/services.dart';
-//import 'package:color/color.dart';
-import 'package:system_shortcuts/system_shortcuts.dart';
-//import 'package:web_view/pubdraw.dart';
-
-
+import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
-   //SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft,DeviceOrientation.landscapeRight]),
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-  
-  theme: ThemeData(
-    primaryColor:Colors.black,
-    secondaryHeaderColor:Colors.red,
-    fontFamily: 'Bungee',
-    primaryTextTheme: TextTheme(
-    title: TextStyle(
-      color: Colors.yellow
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      //primaryColor: Colors.blueGrey[900],
+      //canvasColor: Color.fromRGBO(50, 129, 56, 90),
+      //accentColor: Color.fromRGBO(50, 45, 67, 100),
+      fontFamily: 'Bungee',
+      primaryTextTheme: TextTheme(
+        title: TextStyle(color: Colors.yellow),
       ),
-  ),
-)));
-
+    )));
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
-
+  _MyAppState createState() => new _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: AfterSplash(),
+      image: new Image.asset(
+        'assets/images/icon.png',
+        width: 100,
+        height: 100,
+      ),
+      backgroundColor: Colors.blueGrey[800],
+      photoSize: 100.0,
+      loaderColor: Colors.yellow[300],
+    );
+  }
+}
 
+class AfterSplash extends StatefulWidget {
+  @override
+  _MyAppsState createState() => _MyAppsState();
+}
+
+class _MyAppsState extends State<AfterSplash> {
   @override
   Widget build(BuildContext context) {
     return WebviewScaffold(
       appBar: AppBar(
-        title: Text("Crypto Signals"),
-        actions: <Widget>[
-            // action button
-            IconButton(
-              icon: Icon(Icons.screen_rotation),
-  onPressed: () async {
-    await SystemShortcuts.orientLandscape();
-  }),
-   IconButton(
-    
-              icon: Icon(Icons.contacts, color: Colors.white),
-              iconSize: 24,
-  onPressed: () {
-   Navigator.push(
-     context,
-    new MaterialPageRoute(builder:(context) => new Contact()),
-   );}    
-        
-      )
-      ]),
-      
+          title: Text("Crypto Signals"),
+          backgroundColor: Colors.blueGrey[900],
+          ),
       url: "http://crypto.slamtrade.com/",
       withJavascript: true,
       withZoom: true,
+      withLocalStorage: true,
+      enableAppScheme: true,
+      primary: true,
+      supportMultipleWindows: true,
+      allowFileURLs: true,
+      withLocalUrl: true,
+      scrollBar: true,
+      appCacheEnabled: true,
+      userAgent:
+          'https://accounts.google.com/signin/oauth/oauthchooseaccount?client_id=966276189940-1k54igc67tmva1i1tbodfmt3k5kf3e1i.apps.googleusercontent.com&as=bZHn1GRLkEC4_cjzG36PNg&destination=https%3A%2F%2Fslamtrade-ui.firebaseapp.com&approval_state=!ChRHRkRjRUhfeFhDZFMtM3ZQNzZychIfMDZJb0UtTWF2bE1ROEhuU1JuY2dubXFPaVNZSWpoWQ%E2%88%99AJDr988AAAAAXGPAtfiv-BKiA0Kd25fHIHc156Jb1sJX&oauthgdpr=1&xsrfsig=ChkAeAh8TyMGfEj66DNTFSmu0TeDs_vg-UHBEg5hcHByb3ZhbF9zdGF0ZRILZGVzdGluYXRpb24SBXNvYWN1Eg9vYXV0aHJpc2t5c2NvcGU&flowName=GeneralOAuthFlow',
     );
   }
 }
-
-class LoginPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new _LoginPageState();
-}
-
-class _LoginData {
-  String email = '';
-  String password = '';
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Feedback'),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(20.0),
-        child: new Form(
-          key: this._formKey,
-          child: new ListView(
-            children: <Widget>[
-              new TextFormField(
-                keyboardType: TextInputType.emailAddress, // Use email input type for emails.
-                decoration: new InputDecoration(
-                  hintText: 'you@example.com',
-                  labelText: 'E-mail Address'
-                )
-              ),
-              new TextFormField(
-                obscureText: true, // Use secure text for passwords.
-                decoration: new InputDecoration(
-                  hintText: 'Password',
-                  labelText: 'Enter your password'
-                )
-              ),
-              new Container(
-                width: screenSize.width,
-                child: new RaisedButton(
-                  child: new Text(
-                    'Login',
-                    style: new TextStyle(
-                      color: Colors.white
-                    ),
-                  ),
-                  onPressed: () => null,
-                  color: Colors.blue,
-                ),
-                margin: new EdgeInsets.only(
-                  top: 20.0
-                ),
-              )
-            ],
-          ),
-        )
-      ),
-    );
-  }
-}
-
-class Contact extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new _ContactState();
-}
-
-class _ContactState extends State<Contact> {
-@override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Contact'),
-        
-      ),
-      body: Center(
-        child: Text('contact@slamtrade.com', style: TextStyle(fontSize: 24),
-      )
-  ));
-  }
-  }
